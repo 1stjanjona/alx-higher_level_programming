a2e55b15066df4d9832c11fb27fedbf6c01ddf17
1stjanjona <jannat.hammouche@outlook.com>
2023-10-30 09:23:15 -0700

    trying to get the big win to make it better!

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * check_cycle - check the lists cycle
+ * @list: the list to be checked
+ * Return: return (0) for no cycle, or return (1) for cycle
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *last, *first;
+
+	if (!list || !list->next)
+	{
+		return (0);
+	}
+	last = list->next;
+	first = list->next->next;
+	while (last && first && first->next)
+	{
+		last = last->next;
+		first = first->next->next;
+		if (last == first)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
