ed95df56a67e36efe3f5f059a7bcbd029d85e2ee
1stjanjona <jannat.hammouche@outlook.com>
2023-11-20 14:26:00 -0800

    fgrb

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,10 +2,9 @@
 #!/usr/bin/python3
 import sys
 def safe_print_integer_err(value):
-    ntgr = True
     try:
         print("{:d}".format(value))
-    except Exception as exp:
+        return True
+    except (ValueError, TypeError) as exp:
         print("Exception:", exp, file=sys.stderr)
-        ntgr = False
-    return ntgr
+        return False
