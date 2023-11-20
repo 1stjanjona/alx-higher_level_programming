dbf4cfe5ddeaf079c37215ab16807e95f87f26c9
1stjanjona <jannat.hammouche@outlook.com>
2023-11-20 13:43:39 -0800

    all tasks at once except last one

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     printed_elements = 0
     try:
-        while y in range(x):
+        for y in range(x):
             print(my_list[y], end='')
             printed_elements += 1
     except IndexError:
