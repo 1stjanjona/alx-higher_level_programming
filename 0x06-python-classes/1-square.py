045f307858868aee08aa55df13338cf2872594f9
1stjanjona <jannat.hammouche@outlook.com>
2023-11-21 05:42:36 -0800

    square size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Class Square."""
+
+
+class Square:
+    """Define Square."""
+    def __init__(self, size):
+        """Constructor.
+
+        Args:
+            size: size of square.
+        """
+        self.__size = size
