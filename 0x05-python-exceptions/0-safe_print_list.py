6a4d0dea4a4d1636ba0d731c13d454bde46c0c0a
1stjanjona <jannat.hammouche@outlook.com>
2023-11-22 09:54:59 -0800

    new

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,10 +4,10 @@
 def safe_print_list(my_list=[], x=0):
     try:
         y = 0
-        for y in range(x):
+        for y in range(min(x, len(my_list))):
             print(my_list[y], end="")
         print()
-        return y + 1
+        return min(x, len(my_list))
     except IndexError:
         print()
         return y
