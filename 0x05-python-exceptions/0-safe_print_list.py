e01e603a1ddff4aa5952e3a96228bf78fe4af160
1stjanjona <jannat.hammouche@outlook.com>
2023-11-21 16:15:42 -0800

    nedf

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,9 +5,9 @@ def safe_print_list(my_list=[], x=0):
     try:
         for y in range(x):
             print(my_list[y], end="")
-            x += 1
+            y += 1
         print()
-        return x
+        return y
     except IndexError:
         print()
-        return x
+        return y
