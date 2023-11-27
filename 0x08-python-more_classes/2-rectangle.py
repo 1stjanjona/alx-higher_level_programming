9da242ec6e3cf5ceee4d5398e12cc9750ef73b91
1stjanjona <jannat.hammouche@outlook.com>
2023-11-27 11:57:07 -0800

    0 to 7

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -5,16 +5,22 @@
 class Rectangle:
     """Define a rectangle."""
     def __init__(self, width=0, height=0):
-        """Instantiation of rectangle"""
-        self.width = width
-        self.height = height
+        """Instantiation of rectangle
+        Args:
+            width: width of rectangle
+            height: height of rectangle
+        """
+
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
         """to get width"""
         return self.__width
 
-    @width.setter(self, value):
+    @width.setter
+    def width(self, value):
         """set for width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
