33ab0d0d94d215b8f6d19ec78cd859d0ad92f288
1stjanjona <jannat.hammouche@outlook.com>
2023-11-27 13:11:51 -0800

    0 to 7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -8,26 +8,26 @@ class Rectangle:
     number_of_instances = 0
     """the number of instances"""
 
-    print_symbole = '#'
+    print_symbol = '#'
     """printed symbol"""
 
     def __init__(self, width=0, height=0):
-        """Instantiation of rectangle"""
+        """Instantiation of rectangle
+        Args:
+            width: width of rectangle
+            height: hwight of rectangl
+        """
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
-
-    def __del__(self):
-        """Print when instance deleted"""
-        print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
 
     @property
     def width(self):
         """to get width"""
         return self.__width
 
-    @width.setter(self, value):
+    @width.setter
+    def width(self, value):
         """set for width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
@@ -57,13 +57,17 @@ class Rectangle:
         """Return perimeter of rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return self.__width * 2 + self.__height * 2
+        return 2 * (self.__width + self.__height)
 
     def __str__(self):
         """Print rectangle with #"""
         if not self.width or not self.height:
             return ""
-        return ((str(self.print_symbol) * self.width + "\n") * self.height)[:-1]
+        s = ""
+        if self.width and self.height:
+            for x in range(self.__height):
+                s +=  str(self.print_symbol) * self.__width + "\n"
+        return s[:-1]
 
     def __repr__(self):
         """Return string representation of rectangle"""
