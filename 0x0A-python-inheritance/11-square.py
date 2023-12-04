9bf429c9bf9e240a5a7d98d00243fb0d6dcb381b
1stjanjona <jannat.hammouche@outlook.com>
2023-12-04 13:33:25 -0800

    task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''11-square.py Module'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''Square Class for Rectangle'''
+    def __init__(self, size):
+        '''Instantiation with size.
+        Args:
+        - size: must be private positive integer.
+        '''
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        '''Area of square implementation'''
+        return self.__size ** 2
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
