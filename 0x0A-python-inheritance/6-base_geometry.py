05f96b4775eb8c7d93dce2ae3c5891d3c6e9da9c
1stjanjona <jannat.hammouche@outlook.com>
2023-12-04 12:13:57 -0800

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''5-base_geometry.py Module.'''
+
+
+class BaseGeometry:
+    '''Class for public instance'''
+    def area(self):
+        '''Raise exception if area not implemented.'''
+        raise Exception('area() is not implemented')
