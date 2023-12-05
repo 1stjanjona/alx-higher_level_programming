3e15c299deacb9438594eed9673ef83e318a4a1e
1stjanjona <jannat.hammouche@outlook.com>
2023-12-05 08:30:57 -0800

    to_json_string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''3-to_json_string.py Module.'''
+import json
+
+
+def to_json_string(my_obj):
+    '''Return JSON representation of object (String)'''
+    return json.dumps(my_obj)
