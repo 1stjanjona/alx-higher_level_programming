9c3c6431c19926449af1b65ea5a79b4dd1061d29
1stjanjona <jannat.hammouche@outlook.com>
2023-12-05 11:41:24 -0800

    slass_to_json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -6,9 +6,12 @@ import json
 def class_to_json(obj):
     '''Return dictionary description for JSON serialization of an object'''
     if hasattr(obj, '__dict__'):
-        return obj.__dict__.copy()
+        for key, value in obj.__dict__.items():
+            if isinstance(value, (list, dict, str, int, bool)):
+                return {key: value}
     elif hasattr(obj, "__slots__"):
         for key in obj.__slots__:
-            return {key: getatte(obj, key)}
+            if isinstance(getattr(obj, key), (list, dict, str, int, bool)):
+                return {key: getattr(obj, key)}
     else:
         return {}
