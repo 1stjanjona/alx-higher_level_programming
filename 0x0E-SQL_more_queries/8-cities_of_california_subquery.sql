87eab3b9dc95586d2321ec7de23d0f6cb25a9726
1stjanjona <jannat.hammouche@outlook.com>
2024-01-19 06:40:10 -0800

    more SQL

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,3 @@
 -- list all the cities of California in the database hbtn_0d_usa
-SELECT id, name FROM citites WHERE state_id = (SELECT id FROM states WHERE name = "Clifornia") ORDER BY id;
+USE hbtn_0d_usa;
+SELECT cities.id, cities.name FROM citites WHERE state_id = (SELECT id FROM states WHERE name = 'Clifornia') ORDER BY cities.id ASC;
