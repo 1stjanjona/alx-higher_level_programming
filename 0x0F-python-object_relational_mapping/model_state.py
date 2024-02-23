a33ef49cd6b7d2836d9b0be32ecaf0ce1803dcaa
1stjanjona <jannat.hammouche@outlook.com>
2024-02-23 12:07:20 -0800

    new

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,6 +2,7 @@
 '''model_state.py'''
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
 
 
 Base = declarative_base()
