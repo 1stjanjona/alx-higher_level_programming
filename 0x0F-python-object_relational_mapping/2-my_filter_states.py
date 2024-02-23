4983593a5881cf65330951ac797ea30681955103
1stjanjona <jannat.hammouche@outlook.com>
2024-02-23 10:41:10 -0800

    new

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,13 +5,23 @@ import sys
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursos()
-    cur.execute('SELECT * FROM states WHERE name LIKE BINARY '{}''
-                .format(sys.argv[4]))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state_name>".
+            format(sys.argv[0]))
+        sys.exit(1)
+    username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    try:
+        db = MySQLdb.connect(host="localhost", user=username,
+                             passwd=password, db=database, port=3306)
+        cur = db.cursor()
+        query = "SELECT * FROM states WHERE name = %s ORDER BY id;"
+        cur.execute(query, (state_name,))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
+    finally:
+        if db:
+            db.close()
