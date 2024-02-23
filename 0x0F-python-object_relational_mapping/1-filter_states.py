cd0ccd77c4eaf182d92bc147075c1edfab969534
1stjanjona <jannat.hammouche@outlook.com>
2024-02-23 10:01:06 -0800

    up

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,7 +5,7 @@ import sys
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+    '''db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursos()
     cur.execute('''SELECT * FROM states WHERE name LIKE BINARY 'N%'
@@ -14,4 +14,23 @@ if __name__ == "__main__":
     for row in rows:
         print(row)
     cur.close()
-    db.close()
+    db.close()'''
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    try:
+        db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+        cursor = db.cursos()
+        query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id;"
+        cursor.execute(query)
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+    except <ySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
+    finally:
+        if db:
+            db.close()
