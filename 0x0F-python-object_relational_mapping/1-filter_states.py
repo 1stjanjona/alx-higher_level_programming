04ce5f063750d6cc647e96b7ee68c7d4e05e13c4
1stjanjona <jannat.hammouche@outlook.com>
2024-02-23 10:19:42 -0800

    up

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     '''db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursos()
-    cur.execute('''SELECT * FROM states WHERE name LIKE BINARY 'N%'
-                ORDER BY states.id''')
+    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%'
+                ORDER BY states.id""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
@@ -19,16 +19,17 @@ if __name__ == "__main__":
         print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    db = None
     try:
-        db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-        cursor = db.cursos()
+        db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=password, db=database, port=3306)
+        cur = db.cursor()
         query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id;"
-        cursor.execute(query)
-        results = cursor.fetchall()
-        for row in results:
+        cur.execute(query)
+        rows = cur.fetchall()
+        for row in rows:
             print(row)
-    except <ySQLdb.Error as e:
+    except MySQLdb.Error as e:
         print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
         sys.exit(1)
     finally:
