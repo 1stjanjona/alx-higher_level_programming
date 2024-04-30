bfe446540144dbf4af671c23d2ae13531e84425c
1stjanjona <jannat.hammouche@outlook.com>
2024-04-30 13:59:20 +0000

    0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,6 +3,12 @@
 const fs = require('fs');
 // Import the built-in Node.js 'fs' module.
 
+// Check if the file path is provided as a command-line argument
+if (process.argv.length < 3) {
+  console.error('Error: Please provide the file path as a command-line argument.');
+  process.exit(1); // Terminate the script with a non-zero exit code to indicate an error
+}
+
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
   // Use fs.readFile() to read the contents of a file specified as a command-line argument
   // 'utf8' specifies the encoding of the file being read
